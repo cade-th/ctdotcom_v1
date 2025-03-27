3c5e6fc2b8b528ba25e9bc0567254f314c3c0512
Cade-th <cade.thorn9@gmail.com>
2025-03-26 23:35:50 -0500

    initial commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   const meta = {
-    title: "Next.js Blog Starter Kit",
-    description: "Clone and deploy your own Next.js portfolio in minutes.",
+    title: "My personal blog/website",
+    description: "Just some blogs.",
     image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
   };
 
